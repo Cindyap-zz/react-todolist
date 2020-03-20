2d40c6b55f9fabb6e9f132e391afd7767501cae5
Cindyap <51691282+Cindyap@users.noreply.github.com>
2020-03-20 14:24:33 +0700

    add-jsx

--- src/App.js
+++ src/App.js
@@ -3,9 +3,54 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div className="container">
+      <div className="frame">
+        <section className="header">
+          <button className="header-bttn main-black-color">Add</button>
+          <h1 className="header-tittle">To Do List</h1>
+          <button className="header-bttn main-red-color">Clear</button>
+        </section>
+
+        <section className="add">
+          <form className="add-form">
+            <input className="add-input" type="text" />
+            <button className="add-bttn main-black-color">Add</button>
+          </form>
+        </section>
+
+        <section className="todos">
+          <div className="todo">
+            <span className="todotext">Learn-React</span>
+          </div>
+          <div className="todo">
+            <span className="todotext">Learn-React</span>
+          </div>
+          <div className="todo">
+            <span className="todotext">Learn-React</span>
+          </div>
+          <div className="todo">
+            <span className="todotext">Learn-React</span>
+          </div>
+          <div className="todo">
+            <span className="todotext">Learn-React</span>
+          </div>
+          <div className="todo">
+            <span className="todotext">Learn-React</span>
+          </div>
+          <div className="todo">
+            <span className="todotext">Learn-React</span>
+          </div>
+          <div className="todo">
+            <span className="todotext">Learn-React</span>
+          </div>
+          <div className="todo">
+            <span className="todotext">Learn-React</span>
+          </div>
+          <div className="todo">
+            <span className="todotext">Learn-React</span>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
